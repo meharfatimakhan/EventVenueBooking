ee3d90f0301399d4769eb1eed7c5b1920f14caad
meharfatimak <mehar.fatima@tintash.com>
2024-03-01 13:24:57 +0000

    Add validated instance.xmi file

--- gendev.hw1/src/gendev/hw1/Task4.java
+++ gendev.hw1/src/gendev/hw1/Task4.java
@@ -28,6 +28,16 @@ public class Task4 {
         customer2.setName("Nikhil");
         customer2.setEmail("nikhil@gmail.com");
         customer2.setPhone("0091665432");
+        
+        Customer customer3 = Hw1Factory.eINSTANCE.createCustomer();
+        customer3.setName("Ava");
+        customer3.setEmail("ava@gmail.com");
+        customer3.setPhone("0091665442");
+        
+        Customer customer4 = Hw1Factory.eINSTANCE.createCustomer();
+        customer4.setName("Kakashi");
+        customer4.setEmail("kakashi@gmail.com");
+        customer4.setPhone("0091663542");
 
         Review review1 = Hw1Factory.eINSTANCE.createReview();
         review1.setRating(4);
@@ -46,10 +56,10 @@ public class Task4 {
         venue1.setCapacity(100);
         venue1.setVenueID(1);
         
-//        Venue venue2 = Hw1Factory.eINSTANCE.createVenue();
-//        venue2.setLocation("GRU Hall, Leicester LE4198");
-//        venue2.setCapacity(50);
-//        venue2.setVenueID(2);
+        Venue venue2 = Hw1Factory.eINSTANCE.createVenue();
+        venue2.setLocation("GRU Hall, Leicester LE4198");
+        venue2.setCapacity(500);
+        venue2.setVenueID(2);
 
         EventBooking eventBooking = Hw1Factory.eINSTANCE.createEventBooking();
         eventBooking.setEventDescription("A birthday party of a boy 10 year old with toy story theme");
@@ -71,17 +81,27 @@ public class Task4 {
         eventBooking2.setBookingStatus("Confirmed");
         eventBooking2.setNumberOfGuests(80);
         eventBooking2.setBookingID(3);
+        
+        EventBooking eventBooking3 = Hw1Factory.eINSTANCE.createEventBooking();
+        eventBooking3.setEventDescription("A sports event");
+        eventBooking3.setEstimatedPriceQuote(900);
+        eventBooking3.setBookingBy(customer4);
+        eventBooking3.setBookingDate("Mon Feb 26 10:30:00 GMT 2024");
+        eventBooking3.setBookingType("Event");
+        eventBooking3.setBookingStatus("Confirmed");
+        eventBooking3.setNumberOfGuests(290);
+        eventBooking3.setBookingID(4);
 
         BookingAdmin bookingAdmin = Hw1Factory.eINSTANCE.createBookingAdmin();
         bookingAdmin.setAdminID(1);
         bookingAdmin.setEmail("ava@gmail.com");
         bookingAdmin.setPhone("9876543210");
-        bookingAdmin.setNumberOfApprovals(1);
+        bookingAdmin.setNumberOfApprovals(2);
         bookingAdmin.setName("Ava John");
 
         VenueVisitBooking venueVisitBooking = Hw1Factory.eINSTANCE.createVenueVisitBooking();
         venueVisitBooking.setVisitPurpose("Lark Venue Visit");
-        venueVisitBooking.setBookingBy(customer2);
+        venueVisitBooking.setBookingBy(customer3);
         venueVisitBooking.setBookingDate("Mon Feb 28 10:30:00 GMT 2024");
         venueVisitBooking.setBookingStatus("Confirmed");
         venueVisitBooking.setBookingType("Visit");
@@ -92,19 +112,17 @@ public class Task4 {
         // Establish links between objects
         customer1.getSubmits().add(review1);
         customer1.getSubmits().add(review2);
-        venue1.getBookings().add(eventBooking);
         venue1.getBookings().add(eventBooking2);
-//        bookingAdmin.getVenuesManaged().add(venue1);
-//        bookingAdmin.getVenuesManaged().add(venue2);
-        bookingAdmin.getApprovedBookings().add(eventBooking);
         bookingAdmin.getApprovedBookings().add(eventBooking2);
+        bookingAdmin.getVenuesManaged().add(venue2);
+        venue2.getBookings().add(venueVisitBooking);
+        venue2.getBookings().add(eventBooking3);
         // Save the model to XMI file
-        saveModel(instance, customer1, review1, venue1, eventBooking, venueVisitBooking, bookingAdmin);
+        saveModel(instance, customer1, venueVisitBooking, bookingAdmin);
     }
 	
 	private static void saveModel(String fileName, Customer customer1, 
-			Review review2, Venue venue2, 
-			EventBooking eventBooking, VenueVisitBooking venueVisitBooking, BookingAdmin bookingAdmin) {
+			 VenueVisitBooking venueVisitBooking, BookingAdmin bookingAdmin) {
 	        ResourceSet resourceSet = new ResourceSetImpl();
 
 	        // Register the XMI resource factory
@@ -115,9 +133,9 @@ public class Task4 {
 	        System.out.println("URI" + URI.createURI(fileName));
 	        // Add instances to the resource
 	        resource.getContents().add(customer1);
-	        resource.getContents().add(review2);
-	        resource.getContents().add(venue2);
-	        resource.getContents().add(eventBooking);
+//	        resource.getContents().add(review2);
+//	        resource.getContents().add(venue2);
+//	        resource.getContents().add(eventBooking);
 	        resource.getContents().add(venueVisitBooking);
 	        resource.getContents().add(bookingAdmin);
 	        // Save the resource
